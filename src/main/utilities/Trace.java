b60f0d1e67d4d8e948c238e595cca12302fba488
kyle <kylechen49@gmail.com>
2019-10-30 14:45:25 -0400

    Make support to mac os

--- src/main/utilities/Trace.java
+++ src/main/utilities/Trace.java
@@ -4,25 +4,32 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class Trace {
-	
-	private static Trace _instance = null;		
-	public Logger getLogger (Object o) {
-		return Logger.getLogger(o.getClass().getName());
-	}
-	
-	private Trace() {
-		String userDir = System.getProperty("user.dir");
-		String configFile = String.format("%s\\%s\\%s.properties",userDir, "properties","log4j");
-		PropertyConfigurator.configure(configFile);
-	}
-	
-	public static Trace getInstance() {
-		if (_instance == null) {
-			synchronized (Trace.class) {
-				_instance = new Trace();
-			}
-		}
-		return _instance;
-	}
+
+    private static Trace _instance = null;
+
+    public Logger getLogger(Object o) {
+        return Logger.getLogger(o.getClass().getName());
+    }
+
+    private Trace() {
+        String userDir = System.getProperty("user.dir");
+        String configFile;
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            configFile = String.format("%s/%s/%s.properties", userDir, "properties", "log4j");
+        } else {
+            configFile = String.format("%s\\%s\\%s.properties", userDir, "properties", "log4j");
+
+        }
+        PropertyConfigurator.configure(configFile);
+    }
+
+    public static Trace getInstance() {
+        if (_instance == null) {
+            synchronized (Trace.class) {
+                _instance = new Trace();
+            }
+        }
+        return _instance;
+    }
 
 }
